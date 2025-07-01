b73311428545cbf752905687ebb22f39603461c2
Harini205 <harini.arivu@gmail.com>
2025-07-01 01:18:31 -0500

    fixed loop

--- emotion_detection.py
+++ emotion_detection.py
@@ -18,7 +18,7 @@ def emotion_detector(text_to_analyse):
     max_score = max(anger_score, disgust_score, fear_score, joy_score, sadness_score)
     dominant_emotion = ""
 
-    for key, value in formatted_response['emotionPredictions'][0]['emotion'] :
+    for key, value in formatted_response['emotionPredictions'][0]['emotion'].items() :
         if value == max_score :
             dominant_emotion = key
             break
