10f1487aadc6802ec8dc0f04965d8335b367c443
Harini205 <harini.arivu@gmail.com>
2025-07-01 02:42:43 -0500

    fixed server

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' This is the server for the Emotion Detector application which
+helps analyze the emotions in a provided statement. It also returns the 
+most dominant emotion in the text.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    ''' This function returns a formatted response after analyzing a 
+    particular snippet of text. 
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     dominant = response['dominant_emotion']
@@ -12,10 +19,12 @@ def emotion_detect():
     if dominant is None:
         return "Invalid text! Please try again!"
 
-    return "For the given statement, the system response is {}. The dominant emotion is {}.".format(response, dominant)
+    return (f"For the given statement, the system response is {response}."
+        f"The dominant emotion is {dominant}.")
 
 @app.route("/")
 def render_index_page():
+    ''' This function is to create the landing page '''
     return render_template('index.html')
 
 if __name__ == "__main__":
